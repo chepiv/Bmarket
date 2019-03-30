0e260a9c53172c2ee169f7c72f8afff2d57e0929
Bartek <bartek.blazkow@o2.pl>
2019-03-30 18:44:14 +0100

    password basic encryption

--- src/main/java/com/zpi/bmarket/bmarket/controllers/RegisterController.java
+++ src/main/java/com/zpi/bmarket/bmarket/controllers/RegisterController.java
@@ -5,11 +5,24 @@ import com.zpi.bmarket.bmarket.PostRegisterStatus;
 import com.zpi.bmarket.bmarket.domain.User;
 import com.zpi.bmarket.bmarket.repositories.UserDTO;
 import com.zpi.bmarket.bmarket.repositories.UserRepository;
+import com.zpi.bmarket.bmarket.services.Encryption;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import static org.apache.tomcat.util.codec.binary.Base64.encodeBase64;
 
 @Controller
 public class RegisterController {
@@ -27,6 +40,7 @@ public class RegisterController {
     @RequestMapping(value = "/postRegister" ,method = RequestMethod.POST)
     public String postRegister(@ModelAttribute UserDTO userDTO,Model model) {
         // TODO: 30.03.2019 in case of error do not redirect to post register view
+        //check for password matching
         if(!userDTO.getPassword().equals(userDTO.getMatchingPassword())) {
             model.addAttribute("status", PostRegisterStatus.PASSWORDS_NOT_MATCH);
             return "postRegisterView";
@@ -34,14 +48,14 @@ public class RegisterController {
         //create user from user DTO
         User user = new User();
         user.setLogin(userDTO.getLogin());
-        // TODO: 30.03.2019 hash function on password here
-        user.setPassword(userDTO.getPassword());
+        user.setPassword(Encryption.encrypt(userDTO.getPassword()));
         user.setEmail(userDTO.getEmail());
         //add user to database
-        userRepository.save(user);
+        User s = userRepository.save(user);
 
         model.addAttribute("user",user);
         model.addAttribute("status", PostRegisterStatus.SUCCESS);
         return "postRegisterView";
     }
+
 }
