91e3b9046333f80ebc4d8bad81e717c866b787ee
barte <bartek.blazkow@o2.pl>
2019-04-01 12:16:55 +0200

    change UserDTO to RegisterDTO change PostRegisterStatus to PostStatus

---
+++ src/main/java/com/zpi/bmarket/bmarket/PostStatus.java
@@ -0,0 +1,15 @@
+package com.zpi.bmarket.bmarket;
+
+public enum PostStatus {
+    SUCCESS,
+    ERROR,
+    DATABASE_ERROR,
+    WRONG_PASSWORD,
+    WRONG_USERNAME,
+    WRONG_PASSWORD_OR_USERNAME,
+    WEAK_PASSWORD,
+    EMAIL_NOT_VALID,
+    LOGIN_DUPLICATE,
+    EMAIL_DUPLICATE,
+    PASSWORDS_NOT_MATCH
+}
