730ead9931bea38b2b7653e2ece6dddeadc04089
Cezar Ciepielewski <cezar.ciepielewski@gmail.com>
2019-04-01 12:51:24 +0200

    Working login with partial session

--- src/main/java/com/zpi/bmarket/bmarket/controllers/LoginController.java
+++ src/main/java/com/zpi/bmarket/bmarket/controllers/LoginController.java
@@ -2,8 +2,10 @@ package com.zpi.bmarket.bmarket.controllers;
 
 import com.zpi.bmarket.bmarket.DTO.LoginDTO;
 import com.zpi.bmarket.bmarket.PostLoginStatus;
+import com.zpi.bmarket.bmarket.domain.User;
 import com.zpi.bmarket.bmarket.repositories.UserRepository;
 import com.zpi.bmarket.bmarket.services.Encryption;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -27,17 +30,21 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/postLogin", method = RequestMethod.POST)
-    public String postLogin(@ModelAttribute LoginDTO loginDTO, Model model){
+    public String postLogin(@ModelAttribute LoginDTO loginDTO, Model model, HttpSession session){
 
         PostLoginStatus status = PostLoginStatus.SUCCESS;
+        User user = null;
 
 
         try {
-            userRepository.findUserByLoginAndPassword(loginDTO.getUsername(), Encryption.encrypt(loginDTO.getPassword()));
+            user = userRepository.findUserByLoginAndPassword(loginDTO.getUsername(), Encryption.encrypt(loginDTO.getPassword()));
         } catch (Exception e) {
             status = PostLoginStatus.WRONG_PASSWORD_OR_LOGIN;
         }
 
+        if (status == PostLoginStatus.SUCCESS){
+            session.setAttribute("userId", user.getId());
+        }
         model.addAttribute("user",loginDTO);
         model.addAttribute("status",status);
         return "postLoginView";
