a4e799bb4b33768be37247ad4c28c8930ed79420
chepiv <chepurin.ivan@gmail.com>
2019-04-27 20:09:31 +0200

    transform enums to tables

--- src/main/java/com/zpi/bmarket/bmarket/domain/Status.java
+++ src/main/java/com/zpi/bmarket/bmarket/domain/Status.java
@@ -1,12 +1,23 @@
 package com.zpi.bmarket.bmarket.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
 /**
  * Created by chepiv on 25/03/2019.
  * Contact: chepurin.ivan@gmail.com
  * Github:chepiv
  */
-public enum Status {
-    ACTIVE,
-    INACTIVE,
-    CANCELLED
+@Entity
+@Getter
+@Setter
+public class Status {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,unique = true,length = 30)
+    private String status;
 }
