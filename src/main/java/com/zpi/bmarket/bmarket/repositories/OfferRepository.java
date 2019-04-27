94ce5e78f6bf4ef55125f5fc2ddce5e1a6c016df
Bartek <bartek.blazkow@o2.pl>
2019-04-27 18:08:02 +0200

    showing offer list

---
+++ src/main/java/com/zpi/bmarket/bmarket/repositories/OfferRepository.java
@@ -0,0 +1,7 @@
+package com.zpi.bmarket.bmarket.repositories;
+
+import com.zpi.bmarket.bmarket.domain.Offer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OfferRepository extends CrudRepository<Offer, Long> {
+}
