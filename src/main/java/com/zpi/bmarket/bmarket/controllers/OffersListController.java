36dba329fa00d029a14f8df3f90eb510aa1e7e83
Bartek <bartek.blazkow@o2.pl>
2019-04-27 18:36:40 +0200

    offers paging

--- src/main/java/com/zpi/bmarket/bmarket/controllers/OffersListController.java
+++ src/main/java/com/zpi/bmarket/bmarket/controllers/OffersListController.java
@@ -1,34 +1,32 @@
 package com.zpi.bmarket.bmarket.controllers;
 
-import com.google.common.collect.Lists;
 import com.zpi.bmarket.bmarket.domain.Offer;
 import com.zpi.bmarket.bmarket.repositories.OfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class OffersListController {
+
     @Autowired
     private OfferRepository offerRepository;
-    private int recordsPerSite = 10;
-    private int sitesCount;
-    ArrayList<Offer> offers;
+
+    private int limit = 3;
 
     @GetMapping("/offers")
     public String offerListStart(Model model) {
-        offers = Lists.newArrayList(offerRepository.findAll());
-        sitesCount = offers.size() / recordsPerSite + 1;
         return "redirect:/offers/1";
     }
 
     @GetMapping("/offers/{index}")
     public String offerList(Model model, @PathVariable("index") int index) {
-//        ArrayList<Offer> offers = Lists.newArrayList(offerRepository.findAll());
+        List<Offer> offers = offerRepository.findAll(PageRequest.of(index - 1, limit)).getContent();
 
         model.addAttribute("offers", offers);
 
