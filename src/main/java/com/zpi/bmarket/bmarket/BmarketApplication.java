077ff28866c7a28c4311d270bbf7561312fa9599
chepiv <chepurin.ivan@gmail.com>
2019-05-20 13:54:44 +0200

    access image from remote server

--- src/main/java/com/zpi/bmarket/bmarket/BmarketApplication.java
+++ src/main/java/com/zpi/bmarket/bmarket/BmarketApplication.java
@@ -1,8 +1,12 @@
 package com.zpi.bmarket.bmarket;
 
+import com.zpi.bmarket.bmarket.services.ContentPathAccessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BmarketApplication extends SpringBootServletInitializer {
@@ -11,4 +15,15 @@ public class BmarketApplication extends SpringBootServletInitializer {
         SpringApplication.run(BmarketApplication.class, args);
     }
 
+
+}
+
+@Configuration
+class StaticResourceConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/files/**").addResourceLocations("file:///C:/Studia/Semestr 6/ZPI/");
+//        registry.addResourceHandler("/files/**").addResourceLocations("file:///var/opt/oss/content");
+        registry.addResourceHandler("/files/**").addResourceLocations(ContentPathAccessor.getFilesPath());
+    }
 }
